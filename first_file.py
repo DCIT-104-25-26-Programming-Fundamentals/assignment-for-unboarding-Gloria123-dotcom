5a12b4a919868a36289060f9fb16de1a68370a11
Wireko Gloria <gwireko@st.ug.edu.gh>
2026-06-26 21:15:37 +0000

    finally upload assignment file

---
+++ first_file.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+program = input("What program are you offering at University of Ghana? ")
+print(f"My name is {name}. I am offering {program}" 
+       f" and I'm happy to start my first coding in python.")
